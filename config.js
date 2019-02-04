67191a250d99170f19e1003cb36908c684351ec4
tommoze <tomas008@gmail.com>
2019-02-04 23:27:51 +0200

    update variable name

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     username: 'your_bot_twitch_account_name',
-    password: 'your_oauth_password',
+    password: 'your_bot_oauth_password',
     channels: [
         '#your_channel_name'
     ],
