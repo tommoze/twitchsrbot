8e4b1bd9368c581a72fa20adbec2bef3534b9fa2
tommoze <tomas008@gmail.com>
2019-02-08 00:15:01 +0200

    not found, move up, move down, layout

--- bot.js
+++ bot.js
@@ -2,13 +2,19 @@ const express = require('express');
 const tmi = require('tmi.js');
 const config = require('./config');
 
+const channels = config.channels;
+const request = config.request;
+const queue = config.queue;
+const current = config.current;
+const queue_limit = config.queue_limit
+
 // Define configuration options
 const opts = {
-  identity: {
-    username: config.username,
-    password: config.password
-  },
-  channels: config.channels
+    identity: {
+        username: config.username,
+        password: config.password,
+    },
+    channels: config.channels,
 };
 
 // Create a client with our options
@@ -23,44 +29,58 @@ client.connect();
 
 let list = [];
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+function format(item) {
+    return `${item.title} @${item.by}`;
+}
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+function onRequest(context, msg) {
+    const title = msg.replace(request, '');
 
-  // If the command is known, let's execute it
-  if (commandName.startsWith(config.req)) {
-    const out = `${msg.replace(config.req, '')} @${context.username}`;
-
-    if (list.includes(out)) {
+    if (list.includes(title)) {
         return;
     }
 
-    list.push(out);
-  } else if (commandName === config.que) {
-    let msg;
+    list.push({
+        title: title,
+        by: context.username
+    });
+}
 
+function onQueue() {
     if (list.length > 1) {
-        const out = list.slice(1, config.que_limit + 1)
-            .map((item, i) => `[${i + 1}] ${item}`)
+        const out = list.slice(1, queue_limit + 1)
+            .map((item, i) => `[${i + 1}] ${format(item)}`)
             .join(' ');
         const count = list.length - 1;
-        msg = `${count} ${count > 1 ? 'songs' : 'song'} in queue. ${out}`
+        return `${count} ${count > 1 ? 'songs' : 'song'} in queue. ${out}`
     } else {
-        msg = `Queue is empty. Use ${config.que} to add song`;
+        return `Queue is empty. Use ${queue} to add song`;
+    }
+}
+
+// Called every time a message comes in
+function onMessageHandler(target, context, msg, self) {
+    if (self) { return; } // Ignore messages from the bot
+
+    // Remove whitespace from chat message
+    const commandName = msg.trim();
+    let out;
+
+    // If the command is known, let's execute it
+    if (commandName.startsWith(request)) {
+        onRequest(context, msg);
+    } else if (commandName === queue) {
+        out = onQueue();
+    } else if (commandName === current && list.length > 0) {
+        out = format(list[0]);
     }
 
-    client.say(target, msg);
-  } else if (commandName === config.cur && list.length > 0) {
-    client.say(target, list[0]);
-  }
+    out && client.say(target, out);
 }
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+function onConnectedHandler(addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
 }
 
 // Run  http server
@@ -68,9 +88,28 @@ const app = express();
 app.set('view engine', 'pug')
 const port = 3000;
 
-app.get('/delete', function(req, res) {
-    list.splice(req.param.i, 1);
+app.get('/delete', function (req, res) {
+    const out = list.splice(req.query.index, 1);
+
+    if (req.query.notfound) {
+        client.say(channels[0], `${format(out[0])} was not found :(`);
+    }
+
     res.send({});
+});
+
+app.get('/moveup', function(req, res) {
+    if (parseInt(req.query.index) !== 0) {
+        list.splice(req.query.index - 1, 0, list.splice(req.query.index, 1)[0]);
+        res.send({});
+    }
+});
+
+app.get('/movedown', function(req, res) {
+    if (parseInt(req.query.index) !== list.length) {
+        list.splice(req.query.index + 1, 0, list.splice(req.query.index, 1)[0]);
+        res.send({});
+    }
 });
 
 app.get('/', (req, res) => {
--- config.js
+++ config.js
@@ -2,10 +2,10 @@ module.exports = {
     username: 'your_bot_twitch_account_name',
     password: 'your_bot_oauth_password',
     channels: [
-        '#your_channel_name'
+        '#your_twitch_channel'
     ],
-    req: '!req',
-    que: '!que',
-    cur: '!cur',
-    que_limit: 3
+    request: '!req',
+    queue: '!que',
+    current: '!cur',
+    queue_limit: 3
 };
