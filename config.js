7bee8dc0686269f1965e4ecb25133031a5865469
tommoze <tomas008@gmail.com>
2019-02-13 21:37:29 +0200

    cleanup config

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
 module.exports = {
-    username: 'your_bot_twitch_account_name',
-    password: 'your_bot_oauth_password',
-    channels: [
-        '#your_twitch_channel'
-    ],
     request: '!req',
     queue: '!que',
     current: '!cur',
